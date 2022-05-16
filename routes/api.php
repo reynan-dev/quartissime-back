1915176030e5e2a8a70a255c7956cef2ab9c2123
Reynan Vieira <vieira@reynan.dev>
2022-05-16 15:52:34 +0200

    last commit 16/05

--- routes/api.php
+++ routes/api.php
@@ -2,27 +2,7 @@
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
-use App\Http\Controllers\AssociationController;
-use App\Http\Controllers\CommitteeController;
-use App\Http\Controllers\EventController;
 
-
-
-Route::get('/', function () {
-    return view('welcome');
-});
-
-Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
-
-Route::apiResources([
-    '/events' => EventController::class,
-    '/associations' => AssociationController::class,
-    '/committees' => CommitteeController::class,
-]);
-
-Route::get('/events/{association_id}', [EventController::class, 'showAll']);
-
-Route::get('/associations/{committee_id}', [AssociationController::class, 'showAll']);
 
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,27 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\AssociationController;
+use App\Http\Controllers\CommitteeController;
+use App\Http\Controllers\EventController;
 
+
+
+Route::get('/', function () {
+    return view('welcome');
+});
+
+Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
+
+Route::apiResources([
+    '/events' => EventController::class,
+    '/associations' => AssociationController::class,
+    '/committees' => CommitteeController::class,
+]);
+
+Route::get('/events/{association_id}', [EventController::class, 'showAll']);
+
+Route::get('/associations/{committee_id}', [AssociationController::class, 'showAll']);
 
 Route::get('/', function () {
     return view('welcome');
