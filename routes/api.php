3328f82bbc9c78e880920539816fc9316ddcad60
StephDM06 <dumasmiton.stephane@ecomail.fr>
2022-05-17 19:02:35 +0200

    Commit 1705

--- routes/api.php
+++ routes/api.php
@@ -2,6 +2,9 @@
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\AssociationController;
+use App\Http\Controllers\CommitteeController;
+use App\Http\Controllers\EventController;
 
 
 
@@ -18,7 +21,11 @@ Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
 });
 
-
+Route::apiResources([
+    '/events' => EventController::class,
+    '/associations' => AssociationController::class,
+    '/committees' => CommitteeController::class,
+]);
 
 Route::post(
     '/register',
--- routes/web.php
+++ routes/web.php
@@ -13,11 +13,11 @@ Route::get('/', function () {
 
 Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
 
-Route::apiResources([
-    '/events' => EventController::class,
-    '/associations' => AssociationController::class,
-    '/committees' => CommitteeController::class,
-]);
+// Route::apiResources([
+//     '/events' => EventController::class,
+//     '/associations' => AssociationController::class,
+//     '/committees' => CommitteeController::class,
+// ]);
 
 Route::get('/events/{association_id}', [EventController::class, 'showAll']);
 
