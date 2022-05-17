7c6b5afee94e79488f0df3231559a8517be87ff6
hanenetriaa <hanene.triaa@live.fr>
2022-05-17 14:59:42 +0200

    cors

--- routes/api.php
+++ routes/api.php
@@ -8,6 +8,11 @@ Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
 });
 
-Route::get('/register', function (Request $request) {
-    return $request->register();
-});
+
+
+Route::post(
+    '/register',
+    function (Request $request) {
+        return response(205);
+    }
+);
