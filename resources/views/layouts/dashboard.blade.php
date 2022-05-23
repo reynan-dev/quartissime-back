177b7fd84fb078ca48a30685433343982f265c09
Reynan Vieira <vieira@reynan.dev>
2022-05-23 10:30:06 +0200

    commit

---
+++ resources/views/layouts/dashboard.blade.php
@@ -0,0 +1,36 @@
+<!DOCTYPE html>
+<html lang="fr">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Dashboard - Quartissime</title>
+
+    <link rel="stylesheet" href="{{ asset('style.css') }}">
+
+</head>
+
+<body>
+
+    <nav class="sidebar">
+        <h1>sidebar</h1>
+    </nav>
+
+    <div class="content">
+        <section class="newRequest">
+            <h1>new request</h1>
+        </section>
+
+        <section class="associations">
+            <h1>associations</h1>
+        </section>
+
+        <section class="events">
+            <h1>events</h1>
+        </section>
+    </div>
+
+</body>
+
+</html>
--- routes/web.php
+++ routes/web.php
@@ -5,19 +5,13 @@ use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\AssociationController;
 use App\Http\Controllers\CommitteeController;
 use App\Http\Controllers\EventController;
-
+use App\Http\Controllers\DashboardController;
 
 Route::get('/', function () {
     return view('welcome');
 });
 
 Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
-
-// Route::apiResources([
-//     '/events' => EventController::class,
-//     '/associations' => AssociationController::class,
-//     '/committees' => CommitteeController::class,
-// ]);
 
 Route::get('/events/{association_id}', [EventController::class, 'showAll']);
 
@@ -30,4 +24,32 @@ Route::get('/', function () {
 // Route::post(
 //     '/associations', [AssociationController::class, 'store']
 // );
+
+Route::group(['prefix'=>'dashboard','as'=>'dashboard'], function(){
+
+    Route::get('/', ['as' => 'index', 'uses' => 'DashboardController@index']);
+
+    Route::group(['prefix'=>'committee','as'=>'committee'], function(){
+        Route::get('/{id}', ['as' => 'show', 'uses' => 'CommitteeController@index']);
+        Route::get('/edit', ['as' => 'edit', 'uses' => 'CommitteeController@edit']);
+        Route::put('/edit/{id}', ['as' => 'update', 'uses' => 'CommitteeController@update']);
+    });
+
+    Route::group(['prefix'=>'associations','as'=>'associations'], function(){
+        Route::get('/', ['as' => 'index', 'uses' => 'AssociationController@index']);
+        Route::get('/{id}', ['as' => 'show', 'uses' => 'AssociationController@show']);
+        Route::get('/edit', ['as' => 'edit', 'uses' => 'AssociationController@edit']);
+        Route::put('/edit/{id}', ['as' => 'update', 'uses' => 'AssociationController@update']);
+        Route::delete('/{id}', ['as' => 'destroy', 'uses' => 'CommitteeController@destroy']);
+    });
+
+    Route::group(['prefix'=>'events','as'=>'events'], function(){
+        Route::get('/', ['as' => 'index', 'uses' => 'EventController@index']);
+        Route::get('/{id}', ['as' => 'show', 'uses' => 'EventController@show']);
+        Route::get('/edit', ['as' => 'edit', 'uses' => 'EventController@edit']);
+        Route::put('/edit/{id}', ['as' => 'update', 'uses' => 'EventController@update']);
+        Route::delete('/{id}', ['as' => 'destroy', 'uses' => 'EventController@destroy']);
+    });
+
+});
  
